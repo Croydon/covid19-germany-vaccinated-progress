642b356ccc3d4d9e9d6c5ccef5d20c03ebfaea50
Croydon <git@cr0ydon.com>
2021-03-09 22:04:33 +0100

    Init

---
+++ spider.py
@@ -0,0 +1,27 @@
+import scrapy
+import os 
+
+
+class VacinatedSpider(scrapy.Spider):
+    name = 'vacinatedspider'
+    start_urls = ['https://impfdashboard.de']
+
+    def parse(self, response):
+        with open("progress.txt", "w", encoding="utf-8") as file:
+            text = response.xpath("/html/body/main/section[1]/div/div/div/div/div/div/div[2]/p/span[4]/text()").get()
+            percentage = text.split()[0]
+            percentagerounded = text.split(",")[0]
+
+            fullchars = int(int(percentagerounded)/4)
+            emptychars = 25 - fullchars
+            
+            bar = ""
+            for _ in range(fullchars):
+                bar = "{}{}".format(bar, "█")
+            
+            for _ in range(emptychars):
+                bar = "{}{}".format(bar, "░")
+
+            bar = "{} {} %".format(bar, percentage)
+
+            file.write(bar)
